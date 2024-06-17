4b23b0ec11d33fcee3161ed3969f6dcf2512804b
Aaron Ayeni <aaronayeni135@gmail.com>
2024-06-18 00:52:21 +0100

    advanced.js Q1

--- advanced.js
+++ advanced.js
@@ -32,9 +32,9 @@ unitedKingdom = [
  * Q1. Add some tourist attractions to England
  */
 
-england = null;
-
-// console.log(england);
+england = unitedKingdom.find(country => country.name === "England");
+england.touristAttractions = ["Big Ben", "London Eye", "Stonehenge"];
+console.log(england);
 
 /**
  * Q2. Change the capital of Wales to "Cardiff"
